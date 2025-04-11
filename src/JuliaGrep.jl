03716b51e0d7c0056f5079567bb17ddd156e07ab
Colin Swaney <colinswaney@gmail.com>
2025-04-10 21:05:17 -0400

    Basic working version

--- src/JuliaGrep.jl
+++ src/JuliaGrep.jl
@@ -1,33 +1,42 @@
 module JuliaGrep
 
-function (@main)(ARGS)
-	if length(ARGS) < 1
-		println("Missing required arguments: fname, pattern.")
-        return
-    elseif length(ARGS) < 2
-		println("Missing required arguments: pattern.")
-        return
-	end
-	
-	fname = ARGS[1]
-	pattern = Regex(ARGS[2])
-
-    if !isfile(fname)
-        println("File does not exist.")
-        return
+Base.@ccallable function main(argc::Cint, argv::Ptr{Ptr{UInt8}})::Cint
+    C_ARGS = Vector{String}(undef, argc - 1)
+    for i in 2:argc
+        argptr = unsafe_load(argv, i)
+        arg = unsafe_string(argptr)
+        C_ARGS[i - 1] = arg
     end
 
-	open(fname, "r") do f
-		for (n, line) in enumerate(eachline(f))
-			m = match(pattern, line)
-			if !isnothing(m)
-                print("Line $n: ")
-				printstyled(line[1:m.offset - 1]);
-				printstyled(line[m.offset:m.offset + length(m.match) - 1], color = :magenta);
-				printstyled(line[m.offset + length(m.match):end] * "\n");
-			end
-		end
+	if length(C_ARGS) < 1
+		println(Core.stdout, "Missing required arguments: fname, pattern.")
+        return 1
+    elseif length(C_ARGS) < 2
+		println(Core.stdout, "Missing required arguments: pattern.")
+        return 1
 	end
+	
+	fname = C_ARGS[1]
+	pattern = Regex(C_ARGS[2])
+
+    if !isfile(fname)
+        println(Core.stdout, "File does not exist.")
+        return 1
+    end
+
+	f = open(fname, "r")
+    for (n, line) in enumerate(eachline(f))
+        m = match(pattern, line)
+		if !isnothing(m)
+            print(Core.stdout, "Line $n: ")
+			print(Core.stdout, line[1:m.offset - 1]);
+			print(Core.stdout, "\e[1m\e[38;2;198;120;221m", line[m.offset:m.offset + length(m.match) - 1]);
+			# print(Core.stdout, "\e[38;2;166;173;186m", line[m.offset + length(m.match):end] * "\n");
+			print(Core.stdout, "\e[0m", line[m.offset + length(m.match):end] * "\n");
+		end
+    end
+    return 0
 end
+Base.Experimental.entrypoint(main, (Cint, Ptr{Ptr{UInt8}}))
 
 end # module JuliaGrep
