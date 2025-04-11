fa9d887ecc061e47bc5c5c2267043ddfea1d7250
Colin Swaney <colinswaney@gmail.com>
2025-04-10 21:05:50 -0400

    Clean up

--- src/JuliaGrep.jl
+++ src/JuliaGrep.jl
@@ -31,7 +31,6 @@ Base.@ccallable function main(argc::Cint, argv::Ptr{Ptr{UInt8}})::Cint
             print(Core.stdout, "Line $n: ")
 			print(Core.stdout, line[1:m.offset - 1]);
 			print(Core.stdout, "\e[1m\e[38;2;198;120;221m", line[m.offset:m.offset + length(m.match) - 1]);
-			# print(Core.stdout, "\e[38;2;166;173;186m", line[m.offset + length(m.match):end] * "\n");
 			print(Core.stdout, "\e[0m", line[m.offset + length(m.match):end] * "\n");
 		end
     end
